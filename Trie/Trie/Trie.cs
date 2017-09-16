b7738eb34d0a14e1bbfcda4df9c88f429f41bf9a
lesya-tishencko <lesya.tishencko2012@yandex.ru>
2017-09-16 16:20:13 +0300

    Added tries impl

---
+++ Trie/Trie/Trie.cs
@@ -0,0 +1,95 @@
+﻿namespace Trie
+{
+    public class Trie
+    {
+        public bool Add(string element)
+        {
+            if (element == "") return false;
+
+            Vertex curr = root;
+            bool notAdded = false;
+            foreach (char ch in element.ToCharArray())
+            {
+                if (curr.next[ch] == null)
+                {
+                    notAdded = true;
+                    break;
+                }
+                curr = curr.next[ch];
+            }
+            if (!notAdded && curr.terminal) return false;
+
+            curr = root;
+            foreach (char ch in element.ToCharArray())
+            {
+                if (curr.next[ch] == null)
+                {
+                    curr.next[ch] = new Vertex();
+                }
+                curr.postfixCount++;
+                curr = curr.next[ch];
+            }
+            curr.postfixCount++;
+            curr.terminal = true;
+            return true;
+        }
+
+        public bool Contains(string element)
+        {
+            if (element == "") return false;
+
+            Vertex curr = root;
+            foreach (char ch in element.ToCharArray())
+            {
+                if (curr.next[ch] == null) return false;
+                curr = curr.next[ch];
+            }
+            return curr.terminal;
+        }
+
+        public bool Remove(string element)
+        {
+            if (!Contains(element)) return false;
+
+            Vertex curr = root;
+            curr.postfixCount--;
+            foreach (char ch in element.ToCharArray())
+            {
+                curr.next[ch].postfixCount--;
+                if (curr.next[ch].postfixCount == 0)
+                {
+                    curr.next[ch] = null;
+                    return true;
+                }
+                curr = curr.next[ch];
+            }
+            curr.terminal = false;
+            return true;
+        }
+
+        public int Size() => root.postfixCount;
+
+        public int HowManyStartsWithPrefix(string prefix)
+        {
+            if (prefix == "") return Size();
+
+            Vertex curr = root;
+            foreach (char ch in prefix.ToCharArray())
+            {
+                if (curr.next[ch] == null) return 0;
+                curr = curr.next[ch];
+            }
+            return curr.postfixCount;
+        }
+
+        private class Vertex
+        {
+            public Vertex[] next = new Vertex[upperBoundTableSize];
+            public int postfixCount;
+            public bool terminal;
+        }
+
+        private const int upperBoundTableSize = 256;
+        private Vertex root = new Vertex();
+    }
+}
---
+++ Trie/TrieTests/TrieTests.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Trie;
+
+namespace Trie.Tests
+{
+    [TestClass()]
+    public class TrieTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            var trie = new Trie();
+            Assert.IsTrue(trie.Add("He"));
+            Assert.IsTrue(trie.Add("She"));
+            Assert.IsTrue(trie.Add("His"));
+            Assert.IsTrue(trie.Add("Her"));
+            Assert.IsFalse(trie.Add("He"));
+            Assert.IsFalse(trie.Add(""));
+            Assert.IsTrue(trie.Add("They"));
+            Assert.IsFalse(trie.Add("They"));
+            Assert.IsTrue(trie.Add("Their"));
+        }
+
+        [TestMethod()]
+        public void ContainsTest()
+        {
+            var trie = new Trie();
+            trie.Add("He");
+            trie.Add("She");
+            trie.Add("His");
+            trie.Add("Her");
+            trie.Add("They");
+            trie.Add("Their");
+
+            Assert.IsTrue(trie.Contains("He"));
+            Assert.IsTrue(trie.Contains("She"));
+            Assert.IsTrue(trie.Contains("His"));
+            Assert.IsTrue(trie.Contains("Her"));
+            Assert.IsFalse(trie.Contains("Here"));
+            Assert.IsFalse(trie.Contains("H!s"));
+            Assert.IsTrue(trie.Contains("They"));
+            Assert.IsTrue(trie.Contains("Their"));
+
+            trie.Remove("He");
+            trie.Remove("They");
+            trie.Remove("He");
+            trie.Remove("Her");
+
+            Assert.IsFalse(trie.Contains("He"));
+            Assert.IsTrue(trie.Contains("She"));
+            Assert.IsTrue(trie.Contains("His"));
+            Assert.IsFalse(trie.Contains("Her"));
+            Assert.IsFalse(trie.Contains("They"));
+            Assert.IsTrue(trie.Contains("Their"));
+        }
+
+        [TestMethod()]
+        public void RemoveTest()
+        {
+            var trie = new Trie();
+            trie.Add("He");
+            trie.Add("She");
+            trie.Add("His");
+            trie.Add("Her");
+            trie.Add("Her");
+            trie.Add("They");
+            trie.Add("Their");
+
+            Assert.IsTrue(trie.Remove("He"));
+            Assert.IsFalse(trie.Remove("He"));
+            Assert.IsFalse(trie.Remove("H!s"));
+            Assert.IsTrue(trie.Remove("They"));
+        }
+
+        [TestMethod()]
+        public void SizeTest()
+        {
+            var trie = new Trie();
+            trie.Add("He");
+            trie.Add("She");
+            trie.Add("His");
+            trie.Add("Her");
+            trie.Add("Her");
+            trie.Add("They");
+            trie.Add("Their");
+
+            Assert.AreEqual(6, trie.Size());
+
+            trie.Remove("He");
+            trie.Remove("They");
+            trie.Remove("He");
+
+            Assert.AreEqual(4, trie.Size());
+        }
+
+        [TestMethod()]
+        public void HowManyStartsWithPrefixTest()
+        {
+            var trie = new Trie();
+            trie.Add("He");
+            trie.Add("Her");
+            trie.Add("She");
+            trie.Add("His");
+            trie.Add("They");
+            trie.Add("Their");
+
+            Assert.AreEqual(2, trie.HowManyStartsWithPrefix("He"));
+            Assert.AreEqual(3, trie.HowManyStartsWithPrefix("H"));
+            Assert.AreEqual(2, trie.HowManyStartsWithPrefix("The"));
+
+            trie.Remove("He");
+            trie.Remove("They");
+            trie.Remove("Her");
+
+            Assert.AreEqual(0, trie.HowManyStartsWithPrefix("He"));
+            Assert.AreEqual(1, trie.HowManyStartsWithPrefix("The"));
+            Assert.AreEqual(1, trie.HowManyStartsWithPrefix("H"));
+
+            trie.Add("Here");
+            trie.Add("Her");
+
+            Assert.AreEqual(2, trie.HowManyStartsWithPrefix("Her"));
+        }
+    }
+}
